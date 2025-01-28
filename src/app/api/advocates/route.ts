789849c252a6827413045807195aacef4ea78daa
0xevm1 <0xevm1@gmail.com>
2025-01-27 23:43:22 -0800

    fix(formatting): comment

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -4,7 +4,7 @@ import { advocateData } from "../../../db/seed/advocates";
 
 export async function GET() {
   // Uncomment this line to use a database
-  //const data = await db.select().from(advocates);
+  // const data = await db.select().from(advocates);
 
   // Uncomment this line to use dummy data if database is not set up
    const data = advocateData;
