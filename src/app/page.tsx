e9c499164f75fb576f9b7bd750880fe6915f1e5b
0xevm1 <0xevm1@gmail.com>
2025-01-27 21:26:46 -0800

    fix(ssr): move unnecessary code off of use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,127 +1,16 @@
-"use client";
+import ClientTable from "./components/ClientTable";
 
-import { useEffect, useState } from "react";
-
-interface Advocate {
-  firstName: string;
-  lastName: string;
-  city: string;
-  degree: string;
-  specialties: string[];
-  phoneNumber: string;
-  yearsOfExperience: string;
+async function getAdvocates() {
+  const baseUrl = process.env.VERCEL_URL 
+    ? `https://${process.env.BASE_URL}`
+    : 'http://localhost:3000';
+  const res = await fetch(baseUrl + '/api/advocates', { cache: 'force-cache' });
+  const data = await res.json();
+  return data;
 }
 
-export default function Home() {
-  const [advocates, setAdvocates] = useState<Advocate[]>([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
-
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-      });
-    });
-  }, []);
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm: string = e.target.value;
-
-    const element = document.getElementById("search-term");
-    if (element) {
-      element.innerHTML = searchTerm;
-    }
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.toString().includes(searchTerm) ||
-        advocate.phoneNumber.toString().includes(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
-  };
-
-  const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
-  };
-
-  return (
-    <>
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
-      </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {(filteredAdvocates.length > 0 ? 
-              filteredAdvocates.map((advocate, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{advocate.firstName}</td>
-                    <td>{advocate.lastName}</td>
-                    <td>{advocate.city}</td>
-                    <td>{advocate.degree}</td>
-                    <td>
-                      {advocate.specialties.map((s, j) => (
-                        <div key={j}>{s}</div>
-                      ))}
-                    </td>
-                    <td>{advocate.yearsOfExperience}</td>
-                    <td>{advocate.phoneNumber}</td>
-                  </tr>
-                );
-              }) 
-              : 
-              advocates.map((advocate, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{advocate.firstName}</td>
-                    <td>{advocate.lastName}</td>
-                    <td>{advocate.city}</td>
-                    <td>{advocate.degree}</td>
-                    <td>
-                      {advocate.specialties.map((s, j) => (
-                        <div key={j}>{s}</div>
-                      ))}
-                    </td>
-                    <td>{advocate.yearsOfExperience}</td>
-                    <td>{advocate.phoneNumber}</td>
-                  </tr>
-                );
-            }))}
-        </tbody>
-      </table>
-    </main>
-    </>
-  );
+export default async function Home() {
+  const initialData = await getAdvocates();
+  
+  return <ClientTable initialData={initialData} />;
 }
